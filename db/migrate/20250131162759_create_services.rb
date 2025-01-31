8bc9fa1096eab5a477f7146af4f5fa52fdb0ecaa
talhabutt7 <talhabuttt@gmail.com>
2025-01-31 16:31:56 +0000

    Instead of using location:geography, we’ll manually define the column in the migration file.

--- db/migrate/20250131162759_create_services.rb
+++ db/migrate/20250131162759_create_services.rb
@@ -4,6 +4,10 @@ class CreateServices < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :address
       t.text :description
+      t.geometry :location, type: "point", srid: 4326
+      #t.geometry: This is the correct way to define a PostGIS geometry column in Rails.
+      # type: "point": Specifies the geometry type (e.g., point, polygon).
+      # srid: 4326: Specifies the spatial reference system (WGS84, commonly used for latitude/longitude).
 
       t.timestamps
     end
